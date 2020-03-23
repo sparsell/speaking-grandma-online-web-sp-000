3e40560294cd8a8a562a47bea7725c30164e2c6d
Sonja Parsell <hello@sparsell.com>
2020-03-23 13:21:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,8 @@ def speak_to_grandma(message)
   if message == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif message == message.downcase
-  #  return "HUH?! SPEAK UP, SONNY!"
-  #elsif message == message.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif message == message.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif message == message.upcase
     return "NO, NOT SINCE 1938!"
