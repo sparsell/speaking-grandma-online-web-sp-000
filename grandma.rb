b96ac79331ef992c775a29cb431ab7bdcd3e9570
Sonja Parsell <hello@sparsell.com>
2020-03-23 11:12:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,10 +16,9 @@ def speak_to_grandma(message)
 # and yells back
 # NO, NOT SINCE 1938!
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
   else
         puts "NO, NOT SINCE 1938!"
-# 'I LOVE YOU TOO PUMPKIN!'
+#
   end
 
 end
